006ea2e1381850f0b38de0bbeb7eb63807e1af22
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-06-20 00:18:49 +0530

    jii

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { SocketProvider } from './context/Socketcontext.jsx';
 import { BoardProvider } from './context/BoardContext.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Scans all your React files inside src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
